9f56b770dfa5250415e7107babc4e24da6d0cfcf
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-11-07 15:19:55 +0100

    Clean ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 16:31:12 by deydoux           #+#    #+#             */
-/*   Updated: 2023/11/06 16:36:42 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/11/07 15:19:43 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	unsigned char	*ptr;
+	char	*ptr;
 
 	ptr = s;
 	while (n--)
