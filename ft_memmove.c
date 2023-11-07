ad90b647f2247f231da2f8e1780c87f5b6554dcd
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-11-07 10:16:03 +0100

    Split distinct ft_memcpy and memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 16:55:39 by deydoux           #+#    #+#             */
-/*   Updated: 2023/11/07 10:09:20 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/11/07 10:14:55 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	return (ft_memcpy(dest, src, n));
+	char	*dest_ptr;
+	char	*src_ptr;
+	size_t	i;
+
+	if (!dest || !src)
+		return (NULL);
+	dest_ptr = dest;
+	src_ptr = (void *)src;
+	i = 0;
+	while (i < n)
+	{
+		dest_ptr[i] = src_ptr[i];
+		i++;
+	}
+	return (dest);
 }
