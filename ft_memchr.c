be03cf6c0567fd307f427fda4b8e042721e52b46
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-11-07 16:12:56 +0100

    Concat const declaration and assignation

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 15:37:21 by deydoux           #+#    #+#             */
-/*   Updated: 2023/11/07 15:47:40 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/11/07 16:08:51 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,9 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	const unsigned char	*ptr;
+	const unsigned char	*ptr = s;
 	size_t				i;
 
-	ptr = s;
 	i = 0;
 	while (i < n)
 	{
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,20 +6,18 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 15:57:17 by deydoux           #+#    #+#             */
-/*   Updated: 2023/11/07 16:06:28 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/11/07 16:08:00 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const unsigned char	*s1_ptr;
-	const unsigned char	*s2_ptr;
+	const unsigned char	*s1_ptr = s1;
+	const unsigned char	*s2_ptr = s2;
 	size_t				i;
 
 	if (n == 0)
 		return (0);
-	s1_ptr = s1;
-	s2_ptr = s2;
 	i = 0;
 	while (i < n - 1 && s1_ptr[i] == s2_ptr[i])
 		i++;
