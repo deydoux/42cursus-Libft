870de023485c2fd5e698820216186de8f31c006d
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-11-07 16:41:46 +0100

    Clean ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 10:51:04 by deydoux           #+#    #+#             */
-/*   Updated: 2023/11/07 13:09:33 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/11/07 16:40:42 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	len;
 
 	len = 0;
-	if (size != 0)
+	if (size)
 	{
 		while (len < size - 1 && src[len])
 		{
