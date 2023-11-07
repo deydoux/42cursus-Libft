29ae532102eda7ebcb1e13623fb8898cfc94b79f
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-11-07 10:17:40 +0100

    Revert "Split distinct ft_memcpy and memmove"
    This reverts commit 640ffe4b55c357b01148b573aae9373811430482.

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 16:45:44 by deydoux           #+#    #+#             */
-/*   Updated: 2023/11/07 10:15:20 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/11/07 10:08:36 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,23 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	char	*dest_ptr;
 	char	*src_ptr;
+	size_t	i;
 
 	if (!dest || !src)
 		return (NULL);
 	dest_ptr = dest;
 	src_ptr = (void *)src;
-	while (n--)
-		dest_ptr[n] = src_ptr[n];
+	if (dest < src)
+	{
+		i = 0;
+		while (i < n)
+		{
+			dest_ptr[i] = src_ptr[i];
+			i++;
+		}
+	}
+	else
+		while (n--)
+			dest_ptr[n] = src_ptr[n];
 	return (dest);
 }
