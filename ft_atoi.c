47dc908d6454e2af8fbf22a097bcf51e6bab3ccd
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-11-07 17:18:14 +0100

    Optimize ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 16:43:36 by deydoux           #+#    #+#             */
-/*   Updated: 2023/11/07 17:12:19 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/11/07 17:17:57 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,7 @@ int	ft_atoi(const char *nptr)
 	while (ft_isspace(*nptr))
 		nptr++;
 	if (*nptr == '-' || *nptr == '+')
-		if (*nptr++ == '-')
-			sign = -1;
+		sign -= 2 * (*nptr++ == '-')
 	while (ft_isdigit(*nptr))
 		n = n * 10 + *nptr++ - '0';
 	return (sign * n);
