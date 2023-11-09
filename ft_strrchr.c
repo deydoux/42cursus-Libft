eb38794ff7b583a0498ad0f88a8dbf81ccaa14e3
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-11-09 15:57:57 +0100

    Fix ft_strrchr: cast to unsigned char

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 13:55:44 by deydoux           #+#    #+#             */
-/*   Updated: 2023/11/07 14:58:13 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/11/09 15:56:53 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strrchr(const char *s, int c)
 
 	i = ft_strlen(s) + 1;
 	while (i--)
-		if (s[i] == c)
+		if (s[i] == (unsigned char)c)
 			return ((char *)s + i);
 	return (NULL);
 }
