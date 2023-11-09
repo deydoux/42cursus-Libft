18ff88ea2ad7d32bba33a9f6c316a41fb3b24b90
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-11-09 18:04:07 +0100

    Fix ft_substr: too large allocation

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 17:53:08 by deydoux           #+#    #+#             */
-/*   Updated: 2023/11/08 09:30:33 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/11/09 17:57:13 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	size_t	s_len;
 	char	*sub;
 
 	if (!s)
 		return (NULL);
-	if (ft_strlen(s) < start)
+	s_len = ft_strlen(s);
+	if (s_len < start)
 		return (ft_calloc(1, sizeof(char)));
+	if (s_len - start < len)
+		len = s_len - start;
 	len++;
 	sub = malloc(sizeof(char) * len);
 	if (sub)
