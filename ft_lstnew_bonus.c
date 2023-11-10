6ddd417a6b4caa47b8483dbadabdc388d5a740cb
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-11-10 09:24:16 +0100

    Fix ft_lstnew

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 19:23:26 by deydoux           #+#    #+#             */
-/*   Updated: 2023/11/09 13:36:37 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/11/10 09:22:57 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ t_list	*ft_lstnew(void *content)
 	t_list	*lst;
 
 	lst = malloc(sizeof(t_list));
-	if (lst)
-		lst->content = content;
+	if (!lst)
+		return (NULL);
+	lst->content = content;
+	lst->next = NULL;
 	return (lst);
 }
