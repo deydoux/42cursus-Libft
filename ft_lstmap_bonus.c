fe54ac3dc2deb410253951b199f114ebec4666bd
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-11-13 17:51:07 +0100

    Fix ft_lstmap: del content

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 10:38:17 by deydoux           #+#    #+#             */
-/*   Updated: 2023/11/10 18:56:19 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/11/13 17:29:12 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		new = ft_lstnew(content);
 		if (!new)
 		{
-			free(content);
+			del(content);
 			ft_lstclear(&map, del);
 			return (NULL);
 		}
